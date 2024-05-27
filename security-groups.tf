f42126da055b124761b0eee8ad9966567cec2ca6
sukalyan132 <sukalyan132@gmail.com>
2024-05-27 15:23:44 +0530

    change in code

--- security-groups.tf
+++ security-groups.tf
@@ -8,7 +8,9 @@ resource "aws_security_group" "execute_api_ep_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [
+      "sg-030c5004bc9ee9948",
+    ]
 
   }
 
